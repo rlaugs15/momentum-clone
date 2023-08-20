6d785df3af021f7dd76cde0d918c1c90b757558a
Nunnu <68183848+rlaugs15@users.noreply.github.com>
2023-08-20 15:48:45 +0900

    finish

--- js/weather.js
+++ js/weather.js
@@ -4,13 +4,14 @@ function onGeoOk(position) {
     const lat = position.coords.latitude
     const lon = position.coords.longitude
     console.log('당신의 현재 위치입니다.', lat, lon)
-    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`//&units=metric 화씨를 섭씨로 바꿈
     //따옴표가 아니라 백틱으로 감쌈 ``
+    console.log(url)
     fetch(url)
         .then((Response) => Response.json())
         .then((data) => {
-            const weather = document.querySelector('.weather span first-child')
-            const city = document.querySelector('.weather span last-child')
+            const weather = document.querySelector('.weather span:first-child')
+            const city = document.querySelector('.weather span:last-child')
             city.innerText = data.name
             weather.innerText = `${data.weather[0].main} / ${data.main.temp}`
         })
